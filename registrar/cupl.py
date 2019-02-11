c6f5705e8b6cb84d9689f5e7413cc6f8f35ec26c
lanthora <nierunjie@gmail.com>
2019-02-11 16:42:18 +0800

    Fixed CUPL

--- registrar/cupl.py
+++ registrar/cupl.py
@@ -1,9 +1,8 @@
 from . import urp
 
-
 class CUPL(urp.URP):
     def base_url(self):
         return 'http://urp.cupl.edu.cn/'
 
     def get_captcha_base64(self):
-        pass
+        return None
